0d6c7b0a11b958373ab3cbe42b4f74b214137796
Vlad Ageev <vageev06@gmail.com>
2020-10-01 10:02:15 +0200

    Made validation

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -4,13 +4,14 @@ import Input from '../components/Input'
 import AlertWindow from '../components/AlertWinodw'
 
 import axios from 'axios'
-import { info } from '../context'
+import { info, css } from '../context'
 import '../css/account.css'
 
 const UserUnauthorised = ({ showRegistration, setUserLoggedIn }) => {
   // eslint-disable-next-line
   const emailRegEx = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
   const { lang, domain } = useContext(info);
+  const { red, grey } = useContext(css).colors;
 
   const [isCodeAccepted, setAcceptCode] = useState(false);
   const [showRegField, setShowRegField] = useState(showRegistration);
@@ -22,43 +23,82 @@ const UserUnauthorised = ({ showRegistration, setUserLoggedIn }) => {
   const [alertMessage, setAlertMessage] = useState('Error');
   const [isShowAlertMessage, setShowAlertMessage] = useState(false);
 
+  const [isNameCorrect, setNameCorrect] = useState(true);
+  const [isEmailCorrect, setEmailCorrect] = useState(true);
+  const [isPasswordCorrect, setPasswordCorrect] = useState(true);
+  const [isAddPasswordCorrect, setAddPasswordCorrect] = useState(true);
+
+  function checkCorrect() {
+    let nameC = true;
+    let emailC = true;
+    let passwordC = true;
+    let addPasswordC = true;
+    if ([...name].length < 2) {
+      setNameCorrect(false);
+      nameC = false;
+    }
+    if (!emailRegEx.test(email)) {
+      setEmailCorrect(false);
+      emailC = false;
+    }
+    if ([...password].length < 5) {
+      setPasswordCorrect(false);
+      passwordC = false;
+    }
+    if (password !== confirmPassword) {
+      setAddPasswordCorrect(false);
+      addPasswordC = false;
+    }
+    return { nameC, emailC, passwordC, addPasswordC };
+  }
+
   function changeCode(code) {
     setCode(code);
   }
 
   function changeName(name) {
+    setNameCorrect(true);
     setName(name);
   }
 
   function changeEmail(email) {
+    setEmailCorrect(true);
     setEmail(email);
   }
 
   function changePassword(password) {
+    setPasswordCorrect(true);
     setPassword(password);
   }
 
   function changeConfirmingPassword(password) {
+    setAddPasswordCorrect(true);
     setConfirmPassword(password);
   }
 
   async function authorisate() {
-    const { data } = await axios.post(`http://${domain}/authorisation`, { email, password });
-    switch (data) {
-      case 'Something went wrong':
-        setAlertMessage('Данные введены неверно');
-        openAlert();
-        break;
-      case 'User has not accepted his code':
-        setAcceptCode(true);
-        break;
-      case 'Okay':
-        localStorage.setItem('user', JSON.stringify({ email, pass: password }));
-        setUserLoggedIn(true);
-        break;
-      default:
-        console.error('Something went wrong , do nothing');
-        break;
+    const { emailC, passwordC } = checkCorrect();
+    if (emailC && passwordC) {
+      const { data } = await axios.post(`http://${domain}/authorisation`, { email, password });
+      switch (data) {
+        case 'Something went wrong':
+          setEmailCorrect(false);
+          setPasswordCorrect(false);
+          setAlertMessage('Данные введены неверно');
+          openAlert();
+          break;
+        case 'User has not accepted his code':
+          setAcceptCode(true);
+          break;
+        case 'Okay':
+          localStorage.setItem('user', JSON.stringify({ email, pass: password }));
+          setUserLoggedIn(true);
+          break;
+        default:
+          setAlertMessage('Something went wrong');
+          openAlert();
+          break;
+      }
     }
   }
 
@@ -98,31 +138,34 @@ const UserUnauthorised = ({ showRegistration, setUserLoggedIn }) => {
 
   async function registrate() {
     // TODO: Notice the user about his potential mistakes with registration
-    if (password === confirmPassword) {
-      try {
-        const { data } = await axios.post(`http://${domain}/registrate`, { password, email, name, lang });
-        switch (data) {
-          case 'This user already exists': changeForm(); break; // If user already exists and has already accepted his code
-          case 'Data is not valid':
-            setAlertMessage('Данные введены неверно , попробуйте снова');
-            openAlert();
-            break; // If data is not valid
-          case 'Let user enter the code': setAcceptCode(true); break; // If user hasn't existed yet
-          case 'Code has not been accepted': setAcceptCode(true); break; // If user already exists but hasn't accepted his code yet
-          default:
-            setAlertMessage('Неизвестная ошибка , попробуйте снова');
-            openAlert();
-            break; // default
+    const { nameC, emailC, passwordC, addPasswordC } = checkCorrect();
+    if (nameC && emailC && passwordC && addPasswordC) {
+      if (password === confirmPassword) {
+        try {
+          const { data } = await axios.post(`http://${domain}/registrate`, { password, email, name, lang });
+          switch (data) {
+            case 'This user already exists': changeForm(); break; // If user already exists and has already accepted his code
+            case 'Data is not valid':
+              setAlertMessage('Данные введены неверно , попробуйте снова');
+              openAlert();
+              break; // If data is not valid
+            case 'Let user enter the code': setAcceptCode(true); break; // If user hasn't existed yet
+            case 'Code has not been accepted': setAcceptCode(true); break; // If user already exists but hasn't accepted his code yet
+            default:
+              setAlertMessage('Неизвестная ошибка , попробуйте снова');
+              openAlert();
+              break; // default
+          }
+        }
+        catch ({ message }) {
+          setAlertMessage('Неизвестная ошибка , попробуйте снова');
+          openAlert();
         }
       }
-      catch ({ message }) {
-        setAlertMessage('Неизвестная ошибка , попробуйте снова');
+      else {
+        setAlertMessage('Подтвердите пароль');
         openAlert();
       }
-    }
-    else {
-      setAlertMessage('Подтвердите пароль');
-      openAlert();
     }
   }
 
@@ -135,6 +178,10 @@ const UserUnauthorised = ({ showRegistration, setUserLoggedIn }) => {
     setEmail('');
     setPassword('');
     setConfirmPassword('');
+    setNameCorrect(true);
+    setEmailCorrect(true);
+    setPasswordCorrect(true);
+    setAddPasswordCorrect(true);
   }
 
   function changeForm() {
@@ -150,18 +197,18 @@ const UserUnauthorised = ({ showRegistration, setUserLoggedIn }) => {
         {showRegField ?
           <Fragment>
             <h3>{lang === 'ua' ? 'Реєстрація' : 'Регистрация'}</h3>
-            <Input value={name} input={changeName} placeholder={lang === 'ua' ? 'Ім\'я' : 'Имя'} />
-            <Input value={email} input={changeEmail} placeholder={lang === 'ua' ? 'Пошта' : 'Почта'} />
-            <Input value={password} input={changePassword} placeholder="Пароль" type="password" />
-            <Input value={confirmPassword} input={changeConfirmingPassword} placeholder={lang === 'ua' ? 'Підтвердити пароль' : 'Подтвердить пароль'} type="password" />
+            <Input value={name} input={changeName} placeholder={lang === 'ua' ? 'Ім\'я' : 'Имя'} color={isNameCorrect ? grey : red} />
+            <Input value={email} type="email" input={changeEmail} placeholder={lang === 'ua' ? 'Пошта' : 'Почта'} color={isEmailCorrect ? grey : red} />
+            <Input value={password} input={changePassword} placeholder="Пароль" type="password" color={isPasswordCorrect ? grey : red} />
+            <Input value={confirmPassword} input={changeConfirmingPassword} placeholder={lang === 'ua' ? 'Підтвердити пароль' : 'Подтвердить пароль'} type="password" color={isAddPasswordCorrect ? grey : red} />
             <RedBtn click={registrate} text={lang === 'ua' ? 'Зарегестрировтися' : 'Зарегестрироваться'} />
             <p onClick={changeForm}>{lang === 'ua' ? 'Увійти' : 'Войти'}</p>
           </Fragment>
           :
           <Fragment>
             <h3>{lang === 'ua' ? 'Авторизація' : 'Авторизация'}</h3>
-            <Input value={email} input={changeEmail} placeholder={lang === 'ua' ? 'Пошта' : 'Почта'} />
-            <Input value={password} input={changePassword} placeholder="Пароль" type="password" />
+            <Input value={email} type="email" input={changeEmail} placeholder={lang === 'ua' ? 'Пошта' : 'Почта'} color={isEmailCorrect ? grey : red} />
+            <Input value={password} input={changePassword} placeholder="Пароль" type="password" color={isPasswordCorrect ? grey : red} />
             <RedBtn text={lang === 'ua' ? 'Увійти' : 'Войти'} click={authorisate} />
             <p onClick={changeForm}>{lang === 'ua' ? 'Зарегестрировтися' : 'Зарегестрироваться'}</p>
           </Fragment>
@@ -203,26 +250,6 @@ const Account = () => {
     else {
       setUserLoggedIn(false);
     }
-    // if (user !== undefined) {
-    //   // Checking does user exist in localStorage and if so is its data properly written
-    //   try {
-    //     // Tries to access user's object
-    //     const { name, pass } = user;
-    //     if ([...name].length > 0 && [...pass].length > 0) {
-    //       setUserLoggedIn(true);
-    //     }
-    //     else {
-    //       setUserLoggedIn(false);
-    //     }
-    //   }
-    //   catch (err) {
-    //     // If user's object is inappropriate
-    //     console.log(err.message)
-    //     console.error('This user is not a requied object');
-    //     setUserLoggedIn(false);
-    //   }
-    // }
-    // else setUserLoggedIn(false);
   }, [user, domain]);
 
   return <div className="accountPage"> {isUserLoggedIn ? <UserAuthorised /> : <UserUnauthorised setUserLoggedIn={setUserLoggedIn} showRegistration={true} />} </div>;
